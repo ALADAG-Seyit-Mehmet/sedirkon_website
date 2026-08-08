a3229a260a15bab9faff584dc5c6b9f80b87d1c4
Seyit Mehmet Aladağ <seyit.mehmet.aladag.work@gmail.com>
2026-08-08 18:10:51 +0300

    fix: make hero background image fully visible on mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,7 +26,7 @@ export default function Hero() {
             priority
             quality={100}
             sizes="100vw"
-            className="object-cover object-[25%_center] md:object-center w-full h-full will-change-transform"
+            className="object-contain md:object-cover md:object-center w-full h-full will-change-transform"
           />
         </RevealImage>
         {/* Dark cinematic overlay */}
